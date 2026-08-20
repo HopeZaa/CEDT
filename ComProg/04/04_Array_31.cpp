05589d20ee223ba5fcd79eeadfc6b72c04833a40
HopeZaa <www>
2026-08-20 13:13:25 +0700

    update

--- ComProg/04/04_Array_31.cpp
+++ ComProg/04/04_Array_31.cpp
@@ -5,65 +5,66 @@ typedef long long ll;
 int main(){
     cin.tie(nullptr)->sync_with_stdio(false);cout.tie(0);
     string text, temp = "";
+    int dig[] = {12, 9, 6, 3};
+    string suf[] = {"trillion", "billion", "million", "thousand", ""};
+    string pre[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    string ten[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+    string tens[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    int k=0;
     cin >> text;
-    string num[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    string luksib[] = {"", "-", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-    string sib[] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
     vector<string> v;
-    for(int i=text.length() - 1;i>=0;--i){
+    for(int i=text.length()-1;i>=0;--i){
+        ++k;
         temp += text[i];
-        if(temp.length() == 3 or i == 0){
+        if(k % 3 == 0 or i == 0){
             reverse(temp.begin(), temp.end());
             v.emplace_back(temp);
             temp = "";
         }
     }
     reverse(v.begin(), v.end());
-    if(text.length() > 12){
-        int n = stoi(v.front());
-        if(n != 0){
-            if(v.front().length() == 3 and !v.front().empty()){
-                    
-                cout << num[v.front()[0] - '0'] << " hundred ";
-                v.front().erase(v.front().begin());
+    k = 0;
+    while(!v.empty()){
+        if(dig[k] < v.size() * 3){
+            bool isPrint = false;
+            if(!v.empty()){
+                temp = v.front();
             }
-            if(v.front().length() == 2 and !v.front().empty()){
-                if(stoi(v.front()) != 0){
-                    if(stoi(v.front()) < 10){
-                        cout << num[stoi(v.front())] << ' ';
-                        v.erase(v.begin());
+            if(temp.length() == 3){
+                if(temp[0] != '0'){
+                    isPrint = true;
+                    cout << pre[temp[0] - '0'] << " hundred ";
+                }
+                temp.erase(temp.begin());
+            }
+            if(temp.length() == 2){
+                if(temp[0] != '0'){
+                    if(temp[0] == '1'){
+                        int num = stoi(temp);
+                        isPrint = true;
+                        cout << ten[num % 10] << " ";
+                        temp.erase(temp.begin());                        
                     }
                     else{
-                        if(stoi(v.front()) >= 20){
-                            cout << luksib[stoi(v.front()) / 10] << ' ';
-                            v.front().erase(v.front().begin());
-                            if(stoi(v.front()) != 0){
-                                cout << num[stoi(v.front())] << ' ';
-                                v.erase(v.begin());
-                            }
-                            else{
-                                v.erase(v.begin());
-                            }
-                        }
-                        else{
-                            cout << sib[stoi(v.front()) % 10] << ' ';
-                        }
+                        isPrint = true;
+                        cout << tens[temp[0] - '0'] << " ";
                     }
                 }
-                else{
-                    v.erase(v.begin());
+                temp.erase(temp.begin());
+            }
+            if(temp.length() == 1){
+                if(temp[0] != '0'){
+                    isPrint = true;
+                    cout << pre[temp[0] - '0'] << " ";
                 }
+                temp.erase(temp.begin());
             }
-            if(v.front().length() == 1){
-                cout << num[stoi(v.front())] << ' ';
+            if(isPrint){
+                cout << suf[k] << " ";
             }
-            cout << "trillion ";
+            v.erase(v.begin());
         }
-    }
-    for(auto e : v){
-        cout << e << ' ';
+        // cout << k << " : ";
+        ++k;
     }
 }
-/*
-23999999999999
-*/
