85620f2d16c147576040fa581ee6ea505e70cba7
HopeZaa <www>
2026-09-18 02:17:45 +0700

    update

--- ComProg/2024/2024_Mock_Exam_Midterm_05.cpp
+++ ComProg/2024/2024_Mock_Exam_Midterm_05.cpp
@@ -4,10 +4,28 @@ using namespace std;
 typedef long long ll;
 int main(){
     cin.tie(nullptr)->sync_with_stdio(false);cout.tie(0);
-    int n;
+    int n, cnt = 0;
     cin >> n;
-    vector<int> v(n);
-    for(auto &e : v){
-        cin >> e;
+    string text = "", per = "";
+    for(int i=0;i<n;++i){
+        char a;
+        cin >> a;
+        text += a;
     }
+    vector<string> permu;
+    for(int i=1;i<=n;++i){
+        per += char(i + '0');
+        permu.emplace_back(per);
+    }
+    for(auto e : permu){
+        do{
+            for(int i=0;i<=n-e.size();++i){
+                string sub = text.substr(i, e.size());
+                if(sub == e){
+                    ++cnt;
+                }
+            }
+        }while(next_permutation(e.begin(), e.end()));
+    }
+    cout << cnt;
 }
